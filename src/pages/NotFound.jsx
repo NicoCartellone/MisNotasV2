4cee47b7875a26bb28dbf3329a8bf1c7a4f30794
Nico <nico_cartellone@hotmail.com>
2023-07-10 14:34:44 -0300

    add: NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,12 @@
+import logo from '../assets/tareas.avif'
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <img class='h-auto max-w-full' src={logo} alt='image description' width='70' height='70' />
+      <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+        404: Opps, Page NotFound
+      </h1>
+    </div>
   )
 }
 export default NotFound
