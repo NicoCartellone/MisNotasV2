ea25fa30277ca2f5a1989877a15cccd946e073a7
Nico <nico_cartellone@hotmail.com>
2023-07-10 00:21:59 -0300

    add: Toaster ui sonner in app

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from '../components/Layout/NavBar'
+import { Toaster } from 'sonner'
 
 const Root = () => {
   return (
     <>
+      <Toaster position='top-right' richColors />
       <NavBar />
       <div className='mx-auto mt-10 w-96'>
         <Outlet />
