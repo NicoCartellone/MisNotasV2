22dcd035fd53f03e0d19a3e9db2c7aa33823c9e6
Nico <nico_cartellone@hotmail.com>
2023-07-26 02:15:56 -0300

    add: edit and delete note (not finished)

--- src/components/dashboard/notes/ModalDashboard.jsx
+++ src/components/dashboard/notes/ModalDashboard.jsx
@@ -1,55 +1,18 @@
-import { useEffect, useState } from 'react'
-import { useFirestore } from '../hooks/useFirestore'
-
-import { auth } from '../config/firebase'
-
-import { Card, Button, Checkbox, Label, Modal, TextInput } from 'flowbite-react'
-import ModalNotes from '../components/dashboard/notes/ModalNotes'
-
-import CardDeleteNoteBtn from '../components/dashboard/notes/CardDeleteNoteBtn'
-import EditIcon from '../components/dashboard/icons/EditIcon'
-
-const Dashboard = () => {
-  const { data, getData } = useFirestore()
-
+import { useState } from 'react'
+import { Button, Checkbox, Label, Modal, TextInput } from 'flowbite-react'
+import PlusIcon from '../icons/PlusIcon'
+function ModalDashboard () {
   const [openModal, setOpenModal] = useState()
   const [email, setEmail] = useState('')
   const props = { openModal, setOpenModal, email, setEmail }
-  const [itemData, setItemData] = useState()
-
-  useEffect(() => {
-    getData(auth.currentUser.uid)
-  }, [])
-
   return (
     <>
-      <div className='flex flex-wrap justify-center gap-4 mx-5 my-5 mt-10'>
-        {
-        data.map((item) => (
-          <Card key={item.id} className='max-w-sm min-w-full bg-yellow-200'>
-            <div className='flex justify-between'>
-              <div className='flex flex-col'>
-                <p className='text-2xl font-bold capitalize'>{item.titulo}</p>
-                <p className='text-sm text-gray-700 font-extralight'>{item.fecha}</p>
-              </div>
-              <div className='gap-2'>
-                <button onClick={() => { props.setOpenModal('form-elements'); setItemData(item) }}> <EditIcon /></button>
-                <CardDeleteNoteBtn itemId={item.id} />
-              </div>
-            </div>
-            <p className='text-xl text-center break-words'>{item.nota}</p>
-          </Card>
-        ))
-      }
-      </div>
-      <div className='fixed bottom-4 md:bottom-8 lg:bottom-12 right-4 md:right-8 lg:right-12'>
-        <ModalNotes />
-      </div>
+      <Button pill onClick={() => props.setOpenModal('form-elements')}> <PlusIcon /></Button>
       <Modal show={props.openModal === 'form-elements'} size='md' popup onClose={() => props.setOpenModal(undefined)}>
         <Modal.Header />
         <Modal.Body>
           <div className='space-y-6'>
-            <h3 className='text-xl font-medium text-gray-900 dark:text-white'>{itemData?.titulo}</h3>
+            <h3 className='text-xl font-medium text-gray-900 dark:text-white'>Sign in to our platform</h3>
             <div>
               <div className='block mb-2'>
                 <Label htmlFor='email' value='Your email' />
@@ -86,4 +49,4 @@ const Dashboard = () => {
     </>
   )
 }
-export default Dashboard
+export default ModalDashboard
--- src/components/dashboard/notes/ModalNotes.jsx
+++ src/components/dashboard/notes/ModalNotes.jsx
@@ -1,112 +1,115 @@
 import { useState } from 'react'
-import { Button, Textarea, Label, Modal, TextInput } from 'flowbite-react';
-import PlusIcon from '../icons/PlusIcon';
+import { Button, Textarea, Label, Modal, TextInput } from 'flowbite-react'
+import PlusIcon from '../icons/PlusIcon'
 import { useFirestore } from '../../../hooks/useFirestore'
-import { auth } from '../../../config/firebase';
+import { auth } from '../../../config/firebase'
 
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 
 const addNoteScheme = Yup.object().shape({
-    title: Yup.string()
-        .trim('No se permiten caracteres vacios al inicio y al final')
-        .min(3, 'Minimo 3 caracteres')
-        .max(20, 'Maximo 20 caracteres')
-        .required('Campo obligatorio'),
-    note: Yup.string()
-        .trim('No se permiten caracteres vacios al inicio y al final')
-        .min(3, 'Minimo 3 caracteres')
-        .max(100, 'Maximo 100 caracteres')
-        .required('Campo obligatorio'),
+  title: Yup.string()
+    .trim('No se permiten caracteres vacios al inicio y al final')
+    .min(3, 'Minimo 3 caracteres')
+    .max(20, 'Maximo 20 caracteres')
+    .required('Campo obligatorio'),
+  note: Yup.string()
+    .trim('No se permiten caracteres vacios al inicio y al final')
+    .min(3, 'Minimo 3 caracteres')
+    .max(100, 'Maximo 100 caracteres')
+    .required('Campo obligatorio')
 })
 
 const ModalNotes = () => {
-    const [openModal, setOpenModal] = useState();
-    const props = { openModal, setOpenModal };
+  const [openModal, setOpenModal] = useState()
+  const props = { openModal, setOpenModal }
 
-    const { addData } = useFirestore()
+  const { addData } = useFirestore()
 
-    const handleSubmit = async ({ title, note }) => {
-        await addData(title, note, auth.currentUser.uid)
+  const handleSubmit = async ({ title, note }) => {
+    if (title && note !== '') {
+      await addData(title, note, auth.currentUser.uid)
     }
+  }
 
-    return (
-        <>
-            <Button pill onClick={() => props.setOpenModal('form-elements', ' dismissible')}>
-                <PlusIcon />
-            </Button>
-            <Modal dismissible show={props.openModal === 'form-elements' && 'dismissible'} size="md" popup onClose={() => props.setOpenModal(undefined)}>
-                <Modal.Header />
-                <Modal.Body>
-                    <Formik
-                        initialValues={{ title: '', note: '' }}
-                        validationScheema={addNoteScheme}
-                        onSubmit={handleSubmit}
-                    >
-                        {({
-                            handleChange,
-                            handleBlur,
-                            handleSubmit,
-                            values,
-                            errors
-                        }) => (
-                            <Form className="space-y-6">
-                                <h3 className="text-center text-xl font-medium text-gray-900 dark:text-white">Crear nueva nota</h3>
-                                <div>
-                                    <div className="mb-2 block">
-                                        <Label
-                                            htmlFor="title"
-                                            value="Titulo"
-                                            color={errors.title && 'failure'}
-                                        />
-                                    </div>
-                                    <TextInput
-                                        id="title"
-                                        placeholder='Mi nota'
-                                        required
-                                        type="text"
-                                        name='title'
-                                        onChange={handleChange('title')}
-                                        onBlur={handleBlur('title')}
-                                        value={values.title}
-                                        color={errors.title && 'failure'}
-                                        helperText={errors.title ? <span className='text-red-600'>{errors.title}</span> : null}
-                                    />
-                                </div>
-                                <div>
-                                    <div className="mb-2 block">
-                                        <Label
-                                            htmlFor="note"
-                                            value="Nota"
-                                            color={errors.note && 'failure'}
-                                        />
-                                    </div>
-                                    <Textarea
-                                        id='note'
-                                        placeholder="Escriba su nota aqui..."
-                                        required
-                                        rows={4}
-                                        name='note'
-                                        onChange={handleChange('note')}
-                                        onBlur={handleBlur('note')}
-                                        value={values.note}
-                                        color={errors.note && 'failure'}
-                                        helperText={errors.note ? <span className='text-red-600'>{errors.note}</span> : null}
-                                    />
-                                </div>
-                                <div className="w-full justify-center flex">
-                                    <Button
-                                        type='submit'
-                                        onClick={handleSubmit}
-                                    >Crear</Button>
-                                </div>
-                            </Form>
-                        )}
-                    </Formik>
-                </Modal.Body>
-            </Modal>
-        </>
-    )
+  return (
+    <>
+      <Button pill onClick={() => props.setOpenModal('form-elements', ' dismissible')}>
+        <PlusIcon />
+      </Button>
+      <Modal dismissible show={props.openModal === 'form-elements' && 'dismissible'} size='md' popup onClose={() => props.setOpenModal(undefined)}>
+        <Modal.Header />
+        <Modal.Body>
+          <Formik
+            initialValues={{ title: '', note: '' }}
+            validationScheema={addNoteScheme}
+            onSubmit={handleSubmit}
+          >
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              errors
+            }) => (
+              <Form className='space-y-6'>
+                <h3 className='text-xl font-medium text-center text-gray-900 dark:text-white'>Crear nueva nota</h3>
+                <div>
+                  <div className='block mb-2'>
+                    <Label
+                      htmlFor='title'
+                      value='Titulo'
+                      color={errors.title && 'failure'}
+                    />
+                  </div>
+                  <TextInput
+                    id='title'
+                    placeholder='Mi nota'
+                    required
+                    type='text'
+                    name='title'
+                    onChange={handleChange('title')}
+                    onBlur={handleBlur('title')}
+                    value={values.title}
+                    color={errors.title && 'failure'}
+                    helperText={errors.title ? <span className='text-red-600'>{errors.title}</span> : null}
+                  />
+                </div>
+                <div>
+                  <div className='block mb-2'>
+                    <Label
+                      htmlFor='note'
+                      value='Nota'
+                      color={errors.note && 'failure'}
+                    />
+                  </div>
+                  <Textarea
+                    id='note'
+                    placeholder='Escriba su nota aqui...'
+                    required
+                    rows={4}
+                    name='note'
+                    onChange={handleChange('note')}
+                    onBlur={handleBlur('note')}
+                    value={values.note}
+                    color={errors.note && 'failure'}
+                    helperText={errors.note ? <span className='text-red-600'>{errors.note}</span> : null}
+                  />
+                </div>
+                <div className='flex justify-center w-full'>
+                  <Button
+                    type='submit'
+                    onClick={handleSubmit}
+                  >Crear
+                  </Button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Modal.Body>
+      </Modal>
+    </>
+  )
 }
 
 export default ModalNotes
